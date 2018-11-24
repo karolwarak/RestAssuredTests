8cbb1317989511376c5b9635e5ad24ded4c0f641
Karol Warakomski <k.warakomski1@gmail.com>
2018-11-24 14:00:31 +0100

    check that user is able to browse posts

---
+++ src/test/java/HostnameTest.java
@@ -0,0 +1,15 @@
+public class HostnameTest {
+
+    protected final String hostname;
+
+    HostnameTest(String hostname)
+    {
+        this.hostname = hostname;
+    }
+
+    public String path(String path)
+    {
+        return hostname.concat(path);
+    }
+
+}
---
+++ src/test/java/Jsonplaceholder.java
@@ -0,0 +1,9 @@
+public class Jsonplaceholder extends HostnameTest{
+
+    private static final String HOSTNAME = "https://jsonplaceholder.typicode.com/";
+
+    public Jsonplaceholder()
+    {
+        super(HOSTNAME);
+    }
+}
